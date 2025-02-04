51443c6e96fd07fb675773c001673e540261a3c4
imaadishjain <115503111+imaadishjain@users.noreply.github.com>
2025-02-04 11:27:57 +0530

    Update InterviewList.jsx

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -34,7 +34,7 @@ function InterviewList({ setLoader }) {
   return (
     <div>
       <h2 className="font-medium text-xl mb-5">Mock Interviews</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5">
         {interviewList &&
           interviewList.map((interview, index) => (
             <InterviewItemCart
