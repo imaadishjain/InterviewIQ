686ff38bbc9966b9371ad11a9a66b8c1a6be4ace
imaadishjain <115503111+imaadishjain@users.noreply.github.com>
2025-02-04 11:25:27 +0530

    Update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,22 +2,14 @@
 import React from 'react'
 import Header from './_components/Header'
 import { Toaster } from '@/components/ui/sonner'
-import { HashLoader } from 'react-spinners';
-import { useState } from 'react';
+
 
 function Dashboardlayout({children}) {
-  const[loader,setLoader]=useState(false);
-  if (loader) {
-    return (
-      <div className="flex items-center justify-center mt-20">
-        <HashLoader color="#4845D2" />
-      </div>
-    );
-  }
+ 
 
   return (
     <div>
-     <Header setLoader={setLoader}/>
+     <Header/>
      <div className='mx-5 md:mx-20 lg:mx-36'>
      <Toaster/>
      {children}
